9efbffdfd3b6d9eefb021d10486c1e4d5febc0a3
rjtkatoch007 <rjtkatoch007@gmail.com>
2025-01-12 14:17:33 +0530

    Slider Component design Finished

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import TopNav from "./components/Header/TopNav/TopNav";
 import Menu from "./components/Header/Menu/Menu";
+import Slider from "./components/Slider/Slider";
+import Service from "./components/Service/Service";
 
 export default function Home() {
   return (
@@ -9,6 +11,10 @@ export default function Home() {
       <TopNav/>
       <Menu/>
     </header>
+    <main className="content">
+        <Slider/>
+        <Service/>
+      </main>
 
    </div>
   );
